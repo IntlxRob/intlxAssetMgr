302c4358d9beb91e55c068f6e701c2039bcd3600
Rob Johnston <rob.johnston@Rob-Johnston-MacbookAir.local>
2025-07-21 17:28:02 -0400

    Fix getUserAssets filtering to use string comparison

--- services/zendesk.js
+++ services/zendesk.js
@@ -1,12 +1,16 @@
 const axios = require('axios');
 
 const ZENDESK_BASE_URL = 'https://intlxsolutions.zendesk.com/api/v2';
-const AUTH_TOKEN = process.env.ZENDESK_API_TOKEN; // or hardcode if needed
+const ZENDESK_EMAIL = process.env.ZENDESK_EMAIL;  // e.g. 'rob.johnston@intlxsolutions.com/token'
+const ZENDESK_API_TOKEN = process.env.ZENDESK_API_TOKEN;  // e.g. 'LJ3usrUgoeBZ2fCnGJX2mawtixdr0XnOh7rxPSuI'
 
 const axiosInstance = axios.create({
   baseURL: ZENDESK_BASE_URL,
+  auth: {
+    username: ZENDESK_EMAIL,
+    password: ZENDESK_API_TOKEN,
+  },
   headers: {
-    Authorization: `Bearer ${AUTH_TOKEN}`,
     'Content-Type': 'application/json'
   }
 });
@@ -32,7 +36,6 @@ async function getOrganizations() {
 }
 
 module.exports = {
-  // other exports ...
   getUsers,
   getOrganizations,
 };
